de4a751895e6de04b40875c6a6b23b6e7dbf909a
Anders Holt Knudsen <andhk11@gmail.com>
2020-06-02 08:41:48 +0200

    Finished the Movie App

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MovieCard from "./movieCard";
 
 export default function SearchMovie() {
   // states - input query, movies (it uses destructuring)
@@ -8,6 +9,7 @@ export default function SearchMovie() {
   const searchMovies = async (e) => {
     e.preventDefault();
 
+    // This encapsulates how to fetch data from an API
     const api_key = "584fd4a9317c8321f558652d51f4c099";
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`;
 
@@ -43,23 +45,7 @@ export default function SearchMovie() {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard movie={movie} key={movie.id} />
           ))}
       </div>
     </>
